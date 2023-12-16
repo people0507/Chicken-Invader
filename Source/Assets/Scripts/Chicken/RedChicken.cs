983a36ff9886c7403b556f2781f0256dbe7bdaf0
nguyen210802 <99171296+nguyen210802@users.noreply.github.com>
2023-12-16 15:10:53 +0700

    redchicken 1
    creating red chicken

---
+++ Source/Assets/Scripts/Chicken/RedChicken.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RedChicken : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D myBody;
+    [SerializeField] private float changeDirectionInterval = 3f; // Thời gian đổi hướng
+    private float timer;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = changeDirectionInterval;
+        GetRandomDirection();
+    }
+
+    void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            GetRandomDirection();
+            timer = changeDirectionInterval;
+        }
+    }
+
+    private void GetRandomDirection()
+    {
+        myBody.velocity = new Vector2(Random.Range(-1f, 1f), -speed);
+    }
+}
