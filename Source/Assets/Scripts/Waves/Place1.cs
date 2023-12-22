57617854c47d36c2d7b17e579e3bb8bdfc40f147
Nguyễn Đăng Nhân <2051063581@e.tlu.edu.vn>
2023-12-22 16:10:49 +0700

    fix spawn place

---
+++ Source/Assets/Scripts/Waves/Place1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Place1 : MonoBehaviour
+{
+    GameObject parentObject;
+    private float cameraWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    parentObject = GameObject.Find(transform.parent.name);
+    cameraWidth = -Camera.main.orthographicSize * Camera.main.aspect /2f;
+    Vector3 parentPosition = parentObject.transform.position;
+    Vector3 newPosition = new Vector3(cameraWidth, parentPosition.y, 0);
+    transform.position = newPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
