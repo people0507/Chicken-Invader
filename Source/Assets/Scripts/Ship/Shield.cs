d08340673be7a0474089b2ba72b639f82944729d
Ngoc158 <2051063606@e.tlu.edu.vn>
2023-12-22 22:36:22 +0700

    Update Shield

--- Source/Assets/Scripts/Ship/Shield.cs
+++ Source/Assets/Scripts/Ship/Shield.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     [SerializeField] private float shieldTime;
-    // Start is called before the first frame update
+    private SpriteRenderer spriteRenderer;
+    public bool isShield;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isShield = true;
+    }
     void Start()
     {
-        Destroy(gameObject, shieldTime);
+        Invoke("Hide", shieldTime);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Hide()
     {
-        
+        spriteRenderer.enabled = false;
+        this.isShield = false;
+    }
+    public void Show()
+    {
+        spriteRenderer.enabled = true;
+        this.isShield = true;
+        Start();
     }
 }
